fc472c0e5f9e0cbd8990885b6013a620ee047897
Sin Cheung Ko <scko823@gmail.com>
2016-08-24 11:39:08 -0700

    Quiz component now record all responses for all questions and render records at the end

--- client/js/Quiz.js
+++ client/js/Quiz.js
@@ -10,27 +10,34 @@ mockData.questions.forEach(data=>{
 class Quiz extends React.Component {
 	constructor(){
 		super()
-		this.state={'step':0}
+		this.state={'step':0, responses:[]}
 		console.log(this.state)
 	}
 	incStep(data){
 		console.log(data)
 		if (this.state.step!==mockData.questions.length-1){
+			this.setState({responses: this.state.responses.concat([data])})
 			this.setState({'step': this.state.step+1})
 		}else{
-			this.setState({'step': 0})
+			this.setState({'step': -1})
 		}
 	}
 	render(){
-		return (
-			<Question 
-			question={mockData.questions[this.state.step].question}
-			next={this.incStep.bind(this)} 
-			answer = {mockData.questions[this.state.step].answer}
-			options = {mockData.questions[this.state.step].options}
-			/>
-			
-		)
+		if (~this.state.step){
+				return (
+					<Question
+					question={mockData.questions[this.state.step].question}
+					next={this.incStep.bind(this)}
+					answer = {mockData.questions[this.state.step].answer}
+					options = {mockData.questions[this.state.step].options} />
+				)
+		}else{
+			return (
+				<div>
+					{JSON.stringify(this.state.responses)}
+				</div>
+			)
+		}
 	}
 }
 
