34f157e59e605da19707712840ae876d64b8abbd
Sin Cheung Ko <scko823@gmail.com>
2016-08-24 11:06:53 -0700

    Question component now can select one answer and set the correct state

--- client/js/question.js
+++ client/js/question.js
@@ -1,13 +1,24 @@
 const React = require('react')
 
 class Question extends React.Component {
+	constructor(){
+		super()
+		this.state = {'answer':null}
+	}
+	setAnswer(option){
+		this.setState({'answer':option})
+	}
 	render(){
 		return (
 			<div>
 				<div onClick={this.props.next}>{JSON.stringify(this.props.question)}</div>
 				<form>
 					{this.props.options.map(option=>{
-						return (<div><input type="radio"></input>{option}</div>)
+						return (
+						<div>
+							<input type="radio" checked={option===this.state.answer} onClick={this.setAnswer.bind(this, option)}>
+							</input>{option}
+						</div>)
 					})}
 					<button type="submit">Submit</button>
 				</form>
