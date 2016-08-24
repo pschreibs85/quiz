6c5c2475a75ce18c3c19a6ffd3c398162bbfa25f
Sin Cheung Ko <scko823@gmail.com>
2016-08-24 11:30:32 -0700

    Question component now record studen response to each question and pass back to Quiz component via incStep function

--- client/js/question.js
+++ client/js/question.js
@@ -8,14 +8,25 @@ class Question extends React.Component {
 	setAnswer(option){
 		this.setState({'answer':option})
 	}
+	recordOneQuestion(ev){
+		ev.preventDefault()
+		let data={
+			//add student id, question id?
+			question:this.props.question,
+			answer:this.state.answer,
+			isCorrect: (this.state.answer===this.props.answer)
+		}
+		this.setState({'answer':null})
+		this.props.next(data)
+	}
 	render(){
 		return (
-			<div>
+			<div style={{'textAlign':'center','padding':'10px 20px'}}>
 				<div onClick={this.props.next}>{JSON.stringify(this.props.question)}</div>
-				<form>
-					{this.props.options.map(option=>{
+				<form onSubmit={this.recordOneQuestion.bind(this)} style={{'textAlign':'center'}}>
+					{this.props.options.map((option,i)=>{
 						return (
-						<div>
+						<div key={i}>
 							<input type="radio" checked={option===this.state.answer} onClick={this.setAnswer.bind(this, option)}>
 							</input>{option}
 						</div>)
