434b89237228211605db1a8736230241d23c086d
Sin Cheung Ko <scko823@gmail.com>
2016-08-24 11:02:21 -0700

    rending one form for each question

--- client/js/question.js
+++ client/js/question.js
@@ -3,7 +3,15 @@ const React = require('react')
 class Question extends React.Component {
 	render(){
 		return (
-			<div onClick={this.props.next}>{JSON.stringify(this.props.question)}</div>
+			<div>
+				<div onClick={this.props.next}>{JSON.stringify(this.props.question)}</div>
+				<form>
+					{this.props.options.map(option=>{
+						return (<div><input type="radio"></input>{option}</div>)
+					})}
+					<button type="submit">Submit</button>
+				</form>
+			</div>
 		)
 	}
 }
